a887ffe19385b04be0769f5864755883b5ad00df
sergeysmolkinhtml <37virs85@gmail.com>
2023-06-23 21:44:47 +0200

    System Entities(models, migrations, relations)

---
+++ app/Models/Activity.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'guide_id',
+        'name',
+        'description',
+        'start_time',
+        'price',
+        'photo'
+    ];
+
+    public function company() : \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function participants() : BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -21,6 +22,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
+        'company_id'
     ];
 
     /**
@@ -42,4 +45,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function role() : BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function company() : BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
